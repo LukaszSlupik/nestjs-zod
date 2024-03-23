b59dcb2360169e0977374a8f548dcdfdd9f96464
LukaszSlupik <113589926+LukaszSlupik@users.noreply.github.com>
2024-03-23 15:36:24 +0100

    feat: added validateAsync and createAsyncZodDto functions

--- src/dto.ts
+++ src/dto.ts
@@ -29,6 +29,23 @@ export function createZodDto<
   return AugmentedZodDto as unknown as ZodDto<TOutput, TDef, TInput>
 }
 
+export function createAsyncZodDto<
+  TOutput = any,
+  TDef extends ZodTypeDef = ZodTypeDef,
+  TInput = TOutput
+>(schema: ZodSchema<TOutput, TDef, TInput>) {
+  class AugmentedZodDto {
+    public static isZodDto = true
+    public static schema = schema
+
+    public static create(input: unknown) {
+      return this.schema.parseAsync(input)
+    }
+  }
+
+  return AugmentedZodDto as unknown as ZodDto<TOutput, TDef, TInput>
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isZodDto(metatype: any): metatype is ZodDto<unknown> {
   return metatype?.isZodDto
--- src/validate.ts
+++ src/validate.ts
@@ -21,3 +21,23 @@ export function validate<
 
   return result.data
 }
+
+export async function validateAsync<
+  TOutput = any, // eslint-disable-line @typescript-eslint/no-explicit-any
+  TDef extends ZodTypeDef = ZodTypeDef,
+  TInput = TOutput
+>(
+  value: unknown,
+  schemaOrDto: ZodSchema<TOutput, TDef, TInput> | ZodDto<TOutput, TDef, TInput>,
+  createValidationException: ZodExceptionCreator = createZodValidationException
+) {
+  const schema = isZodDto(schemaOrDto) ? schemaOrDto.schema : schemaOrDto
+
+  const result = await schema.safeParseAsync(value)
+
+  if (!result.success) {
+    throw createValidationException(result.error)
+  }
+
+  return result.data
+}
